ec3f93e307e6820a10d1e4186660448c1325b8cc
Hydra <umeraqeel11@gmail.com>
2020-04-06 21:51:45 +0500

    Java practice

---
+++ src/chapter3/LoanQualifier.java
@@ -0,0 +1,42 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class LoanQualifier {
+
+/*
+ *Nested IFS.
+ * To qualify for loan, a person must make at least $30,000
+ * and have been working at their current job for at least 2 years.
+ */
+    public static void main(String arg[]){
+
+        //Initialize what we know
+        int requiredSalary = 30000;
+        int requiredYearEmployed = 2;
+
+        //Get what we don't
+        System.out.println("Enter your salary:");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.print("Enter the amount of years with your current employer: ");
+        double years = scanner.nextDouble();
+
+        scanner.close();
+
+        //Make decision
+        if (salary >= requiredSalary){
+            if (years >= requiredYearEmployed){
+                System.out.println("Congrats! You qualify for the loan");
+            }
+            else {
+                System.out.println("Sorry you must have worked at your current job " + requiredYearEmployed + " years.");
+            }
+        }
+        else {
+            System.out.println("Sorry you must earn at least $" + requiredSalary + " to qualify for the loan");
+        }
+
+    }
+}
